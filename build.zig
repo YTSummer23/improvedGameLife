cb5858ca544266e27848861b0a9602e5a288bffd
YTSummer23 <smaginmikhail@gmail.com>
2022-04-05 19:34:38 +0000

    tiny change of build.zig

--- build.zig
+++ build.zig
@@ -1,3 +1,21 @@
 const Builder = @import("std").build.Builder;
 
-pub fn build(b: *Builder
+pub fn build(b: *Builder) void {
+    const target = b.standardTargetOptions(.{});
+    const mode = b.standardReleaseOptions();
+
+    const exe = b.addExecutable("sfml", "src/main.zig");
+    exe.linkLibC();
+    exe.addPackagePath("theGame", "sfml-wrapper/src/sfml/sfml.zig");
+    exe.linkSystemLibrary("csfml-graphics");
+    exe.linkSystemLibrary("csfml-system");
+    exe.linkSystemLibrary("csfml-window");
+    exe.linkSystemLibrary("csfml-audio");
+    exe.addIncludeDir("csfml/include/"); // Not always necessary
+    exe.setTarget(target);
+    exe.setBuildMode(mode);
+    exe.install();
+
+    const run_step = b.step("run", "Run the game");
+    run_step.dependOn(&exe.run().step);
+}
